403e9d2cf462b8a4659862a9026160dfa17cd1ca
alexfigliolia <alexfigliolia@gmail.com>
2017-06-05 22:07:26 -0400

    update

--- mobile-config.js
+++ mobile-config.js
@@ -5,4 +5,10 @@ App.appendToConfig(`<platform name="ios">
     <config-file platform="ios" target="*-Info.plist" parent="NSCameraUsageDescription">
       <string>Your camera will scan barcodes and advance you along your punchcard</string>
     </config-file>
+    <config-file parent="UIStatusBarHidden" platform="ios" target="*-Info.plist">
+      <true/>
+	</config-file>
+	<config-file parent="UIViewControllerBasedStatusBarAppearance" platform="ios" target="*-Info.plist">
+	  <false/>
+	</config-file>
   </platform>`);
